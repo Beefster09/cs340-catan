cb37f364f2d1a22f6f94decdcb416ddad975cadc
Steve Pulsipher <steve.pulse5@gmail.com>
2015-09-25 19:27:09 -0600

    Client Communicator
    Filled the function send in the client communicator class

--- src/client/communication/ClientCommunicator.java
+++ src/client/communication/ClientCommunicator.java
@@ -1,6 +1,15 @@
 package client.communication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import shared.exceptions.ServerException;
 
 /**
  * 
@@ -22,7 +31,35 @@ public class ClientCommunicator {
 	 * @post Response from the server will be given
 	 * @return response object from server
 	 */
-	public JSONObject send(JSONObject o){
-		return null;
+	public JSONObject send(JSONObject o) throws ServerException{
+		try{
+			URL url = new URL((String) o.get("url"));
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod((String) o.get("requestType"));
+			con.setDoOutput(true);
+			con.connect();
+			
+			OutputStreamWriter output = new OutputStreamWriter(con.getOutputStream());
+			output.write(o.toString());
+			output.flush();
+			
+			if (con.getResponseCode() == HttpURLConnection.HTTP_OK){
+				InputStream input = con.getInputStream();
+				int len = 0;
+				
+				byte[] buffer = new byte[1024];
+				StringBuilder str = new StringBuilder();
+				while(-1 != (len = input.read(buffer))){
+					str.append(new String(buffer, 0, len));
+				}
+				JSONParser parser = new JSONParser();
+				JSONObject JSONOutput = (JSONObject) parser.parse(str.toString());
+				return JSONOutput;
+			}
+			throw new ServerException();
+		}
+		catch(IOException | ParseException e){
+			throw new ServerException();
+		}
 	}
 }
