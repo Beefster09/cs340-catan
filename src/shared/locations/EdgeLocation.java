8ac5944a7c1859b928884c379e9a122c016938e4
Justin <ifoundthebeef@gmail.com>
2015-09-28 13:53:54 -0600

    Normalized hashCode and equals on VertexLocation and EdgeLocation

--- src/shared/locations/EdgeLocation.java
+++ src/shared/locations/EdgeLocation.java
@@ -61,9 +61,10 @@ public class EdgeLocation
 	public int hashCode()
 	{
 		final int prime = 31;
+		EdgeLocation self = getNormalizedLocation();
 		int result = 1;
-		result = prime * result + ((dir == null) ? 0 : dir.hashCode());
-		result = prime * result + ((hexLoc == null) ? 0 : hexLoc.hashCode());
+		result = prime * result + ((self.dir == null) ? 0 : self.dir.hashCode());
+		result = prime * result + ((self.hexLoc == null) ? 0 : self.hexLoc.hashCode());
 		return result;
 	}
 	
@@ -76,15 +77,16 @@ public class EdgeLocation
 			return false;
 		if(getClass() != obj.getClass())
 			return false;
-		EdgeLocation other = (EdgeLocation)obj;
-		if(dir != other.dir)
+		EdgeLocation other = ((EdgeLocation) obj).getNormalizedLocation();
+		EdgeLocation self = getNormalizedLocation();
+		if(self.dir != other.dir)
 			return false;
-		if(hexLoc == null)
+		if(self.hexLoc == null)
 		{
 			if(other.hexLoc != null)
 				return false;
 		}
-		else if(!hexLoc.equals(other.hexLoc))
+		else if(!self.hexLoc.equals(other.hexLoc))
 			return false;
 		return true;
 	}
--- src/shared/locations/VertexLocation.java
+++ src/shared/locations/VertexLocation.java
@@ -60,9 +60,10 @@ public class VertexLocation
 	public int hashCode()
 	{
 		final int prime = 31;
+		VertexLocation self = getNormalizedLocation();
 		int result = 1;
-		result = prime * result + ((dir == null) ? 0 : dir.hashCode());
-		result = prime * result + ((hexLoc == null) ? 0 : hexLoc.hashCode());
+		result = prime * result + ((self.dir == null) ? 0 : self.dir.hashCode());
+		result = prime * result + ((self.hexLoc == null) ? 0 : self.hexLoc.hashCode());
 		return result;
 	}
 	
@@ -75,15 +76,16 @@ public class VertexLocation
 			return false;
 		if(getClass() != obj.getClass())
 			return false;
-		VertexLocation other = (VertexLocation)obj;
-		if(dir != other.dir)
+		VertexLocation other = ((VertexLocation) obj).getNormalizedLocation();
+		VertexLocation self = getNormalizedLocation();
+		if(self.dir != other.dir)
 			return false;
-		if(hexLoc == null)
+		if(self.hexLoc == null)
 		{
 			if(other.hexLoc != null)
 				return false;
 		}
-		else if(!hexLoc.equals(other.hexLoc))
+		else if(!self.hexLoc.equals(other.hexLoc))
 			return false;
 		return true;
 	}
