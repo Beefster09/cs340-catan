e261754d1dcb81891db732e5cb6d15b5cfec7f06
Steve Pulsipher <steve.pulse5@gmail.com>
2015-10-01 10:32:45 -0600

    Added toJSONObject for ResourceTradeList

--- src/client/communication/ServerProxyTest.java
+++ src/client/communication/ServerProxyTest.java
@@ -2,18 +2,16 @@ package client.communication;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-
 import org.json.simple.JSONObject;
 import org.junit.Test;
 
 import shared.communication.GameHeader;
 import shared.definitions.CatanColor;
+import shared.definitions.ResourceType;
+import shared.locations.EdgeLocation;
 import shared.locations.VertexLocation;
-import shared.model.CatanModel;
-import shared.model.ModelFacade;
 import shared.model.PlayerReference;
-import shared.model.TurnTracker;
+import shared.model.ResourceTradeList;
 
 public class ServerProxyTest {
 	ServerProxy SP;
@@ -24,7 +22,7 @@ public class ServerProxyTest {
 		SP = new ServerProxy();
 		try{
 			SP.register("Steve", "steve");
-			GameHeader game = SP.createGame("test", true, true, true);
+			GameHeader game = SP.createGame("test", false, false, false);
 			SP.joinGame(game.getId(), CatanColor.PURPLE);
 			System.out.println("Registered Steve");
 			
@@ -39,44 +37,235 @@ public class ServerProxyTest {
 			SP.register("Grant", "abc_123");
 			SP.joinGame(game.getId(), CatanColor.ORANGE);
 			System.out.println("Registered Grant");
-			
-			JSONObject modelJSON = SP.getModel(0);
-			
-			CatanModel test = new CatanModel();
-			ModelFacade facade = new ModelFacade(test);
-			facade.updateFromJSON(modelJSON);
-			CatanModel model = facade.getCatanModel();
-			TurnTracker turnTracker = model.getTurnTracker();
+						
+			PlayerReference steve = new PlayerReference(null, 0);
+			PlayerReference justin = new PlayerReference(null, 1);
+			PlayerReference jordan = new PlayerReference(null, 2);
+			PlayerReference grant = new PlayerReference(null, 3);
 			
 			SP.login("Steve", "steve");
-			System.out.println("Logged in as Steve");
 			SP.joinGame(game.getId(), CatanColor.PURPLE);
-			System.out.println("Joined Game");
 
-			
-			PlayerReference user = turnTracker.getCurrentPlayer();
 			JSONObject location = new JSONObject();
 			location.put("x", 0L);
-			location.put("y", 0L);
+			location.put("y", 1L);
+			location.put("direction", "NE");
+			EdgeLocation edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", 1L);
 			location.put("direction", "NW");
 			VertexLocation vertexLocation = new VertexLocation(location);
-			modelJSON = SP.buildSettlement(user, vertexLocation, true);
+			SP.buildRoad(steve, edgeLocation, true);
+			System.out.println("Steve Built a Road");
+			SP.buildSettlement(steve, vertexLocation, true);
 			System.out.println("Steve Built a Settlement");
-			facade.updateFromJSON(modelJSON);
-			model = facade.getCatanModel();
-			turnTracker = model.getTurnTracker();
+			SP.finishTurn(steve);
+			System.out.println("Steve finished his Turn");
 			
-			SP.login("Justin", "123");
-			SP.joinGame(game.getId(), CatanColor.BLUE);
+//			SP.login("Justin", "123");
+//			SP.joinGame(game.getId(), CatanColor.BLUE);
 			
-			user = turnTracker.getCurrentPlayer();
 			location = new JSONObject();
-			location.put("x", 0L);
-			location.put("y", 0L);
+			location.put("x", -1L);
+			location.put("y", 2L);
+			location.put("direction", "NW");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", -1L);
+			location.put("y", 2L);
+			location.put("direction", "W");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(justin, edgeLocation, true);
+			System.out.println("Justin Built a Road");
+			SP.buildSettlement(justin, vertexLocation, true);
+			System.out.println("Justin Built a Settlement");
+			SP.finishTurn(justin);
+			System.out.println("Justin finished his Turn");
+			
+//			SP.login("Jordan", "Jordan");
+//			SP.joinGame(game.getId(), CatanColor.GREEN);
+			
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", 1L);
+			location.put("direction", "NE");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", 1L);
 			location.put("direction", "E");
 			vertexLocation = new VertexLocation(location);
-			modelJSON = SP.buildSettlement(user, vertexLocation, true);
-			System.out.println("Justin Built a Settlement");
+			SP.buildRoad(jordan, edgeLocation, true);
+			System.out.println("Jordan Built a Road");
+			SP.buildSettlement(jordan, vertexLocation, true);
+			System.out.println("Jordan Built a Settlement");
+			SP.finishTurn(jordan);
+			System.out.println("Jordan finished his Turn");
+
+//			SP.login("Grant", "abc_123");
+//			SP.joinGame(game.getId(), CatanColor.ORANGE);
+			
+			location = new JSONObject();
+			location.put("x", -2L);
+			location.put("y", 2L);
+			location.put("direction", "N");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", -2L);
+			location.put("y", 2L);
+			location.put("direction", "NW");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(grant, edgeLocation, true);
+			System.out.println("Grant Built a Road");
+			SP.buildSettlement(grant, vertexLocation, true);
+			System.out.println("Grant Built a Settlement");
+			SP.finishTurn(grant);
+			System.out.println("Grant finished his Turn");
+			
+			location = new JSONObject();
+			location.put("x", -1L);
+			location.put("y", 1L);
+			location.put("direction", "NW");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", -1L);
+			location.put("y", 1L);
+			location.put("direction", "NW");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(grant, edgeLocation, true);
+			System.out.println("Grant Built a 2nd Road");
+			SP.buildSettlement(grant, vertexLocation, true);
+			System.out.println("Grant Built a 2nd Settlement");
+			SP.finishTurn(grant);
+			System.out.println("Grant finished his Turn");
+			
+//			SP.login("Jordan", "Jordan");
+//			SP.joinGame(game.getId(), CatanColor.GREEN);
+			
+			location = new JSONObject();
+			location.put("x", 2L);
+			location.put("y", 0L);
+			location.put("direction", "NW");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", 2L);
+			location.put("y", 0L);
+			location.put("direction", "NW");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(jordan, edgeLocation, true);
+			System.out.println("Jordan Built a 2nd Road");
+			SP.buildSettlement(jordan, vertexLocation, true);
+			System.out.println("Jordan Built a 2nd Settlement");
+			SP.finishTurn(jordan);
+			System.out.println("Jordan finished his Turn");
+
+//			SP.login("Justin", "123");
+//			SP.joinGame(game.getId(), CatanColor.BLUE);
+			
+			location = new JSONObject();
+			location.put("x", -1L);
+			location.put("y", 2L);
+			location.put("direction", "N");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", -1L);
+			location.put("y", 2L);
+			location.put("direction", "NE");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(justin, edgeLocation, true);
+			System.out.println("Justin Built a 2nd Road");
+			SP.buildSettlement(justin, vertexLocation, true);
+			System.out.println("Justin Built a 2nd Settlement");
+			SP.finishTurn(justin);
+			System.out.println("Justin finished his Turn");
+
+//			SP.login("Steve", "steve");
+//			SP.joinGame(game.getId(), CatanColor.PURPLE);
+
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", -1L);
+			location.put("direction", "NW");
+			edgeLocation = new EdgeLocation(location);
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", -1L);
+			location.put("direction", "NW");
+			vertexLocation = new VertexLocation(location);
+			SP.buildRoad(steve, edgeLocation, true);
+			System.out.println("Steve Built a 2nd Road");
+			SP.buildSettlement(steve, vertexLocation, true);
+			System.out.println("Steve Built a 2nd Settlement");
+			SP.finishTurn(steve);
+			System.out.println("Steve finished his Turn");
+
+			SP.rollDice(steve, 4);
+			System.out.println("Steve rolled a 4");
+			location = new JSONObject();
+			location.put("x", 0L);
+			location.put("y", -1L);
+			location.put("direction", "NE");
+			edgeLocation = new EdgeLocation(location);
+			SP.buildRoad(steve, edgeLocation, false);
+			System.out.println("Steve built a 3rd road");
+			
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", -2L);
+			location.put("direction", "NW");
+			edgeLocation = new EdgeLocation(location);
+			SP.buildRoad(steve, edgeLocation, false);
+			System.out.println("Steve built a 4th road");
+			
+			JSONObject tradeJSON = new JSONObject();
+			tradeJSON.put(ResourceType.BRICK.toString().toLowerCase(), 1);
+			tradeJSON.put(ResourceType.ORE.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.SHEEP.toString().toLowerCase(), 1);
+			tradeJSON.put(ResourceType.WHEAT.toString().toLowerCase(), 1);
+			tradeJSON.put(ResourceType.WOOD.toString().toLowerCase(), 0);
+			
+			ResourceTradeList trade = new ResourceTradeList(tradeJSON);
+			
+			SP.offerTrade(steve, trade, jordan);
+			System.out.println("Steve offered trade to Jordan");
+			SP.respondToTrade(jordan, true);
+			System.out.println("Jordan accepted trade");
+			
+			tradeJSON = new JSONObject();
+			tradeJSON.put(ResourceType.BRICK.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.ORE.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.SHEEP.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.WHEAT.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.WOOD.toString().toLowerCase(), 1);
+			
+			trade = new ResourceTradeList(tradeJSON);
+			
+			SP.offerTrade(steve, trade, justin);
+			System.out.println("Steve offered trade to Justin");
+			SP.respondToTrade(justin, false);
+			System.out.println("Justin declined trade");
+	
+			tradeJSON = new JSONObject();
+			tradeJSON.put(ResourceType.BRICK.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.ORE.toString().toLowerCase(), -1);
+			tradeJSON.put(ResourceType.SHEEP.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.WHEAT.toString().toLowerCase(), 0);
+			tradeJSON.put(ResourceType.WOOD.toString().toLowerCase(), 1);
+
+			SP.offerTrade(steve, trade, justin);
+			System.out.println("Steve offered trade to Justin");
+			SP.respondToTrade(justin, true);
+			System.out.println("Justin accepted trade");
+			
+			location = new JSONObject();
+			location.put("x", 1L);
+			location.put("y", -2L);
+			location.put("direction", "NW");
+			vertexLocation = new VertexLocation(location);
+			SP.buildSettlement(steve, vertexLocation, false);
+			System.out.println("Steve built a 3rd settlement");
+
 		}
 		catch(Exception e){
 			e.printStackTrace();
