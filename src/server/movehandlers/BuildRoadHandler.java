d957ecb939442d3df2e55eff8557b3a428627a05
Steve Pulsipher <steve.pulse5@gmail.com>
2015-11-14 19:57:06 -0700

    BuildRoadHandler Works

--- src/server/movehandlers/BuildRoadHandler.java
+++ src/server/movehandlers/BuildRoadHandler.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -15,6 +16,7 @@ import com.sun.net.httpserver.HttpHandler;
 import client.communication.MockServer;
 import server.interpreter.ExchangeConverter;
 import shared.communication.IServer;
+import shared.exceptions.SchemaMismatchException;
 import shared.exceptions.ServerException;
 import shared.exceptions.UserException;
 import shared.locations.EdgeLocation;
@@ -41,13 +43,14 @@ public class BuildRoadHandler extends AbstractMoveHandler implements HttpHandler
 				throw new ServerException();
 			}
 			JSONObject json = ExchangeConverter.toJSON(arg0);
-			/*
-			 * Extract needed information from JSON, and call the appropriate server method.
-			 */
-			EdgeLocation location = (EdgeLocation)json.get("roadLocation");
+
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject = (JSONObject)parser.parse((String)json.get("roadLocation"));
+			EdgeLocation location = new EdgeLocation(jsonObject);
+			
 			boolean free = (boolean)json.get("free");
 			
-			int playerIndex = (int)json.get("playerIndex");
+			int playerIndex = (int)(long)json.get("playerIndex");
 			String gson = server.buildRoad(playerIndex, gameID, location, free);
 			
 			arg0.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
@@ -55,7 +58,7 @@ public class BuildRoadHandler extends AbstractMoveHandler implements HttpHandler
 			output.write(gson.toString());
 			output.flush();
 			arg0.getResponseBody().close();
-		} catch (ParseException | ServerException | UserException e) {
+		} catch (ParseException | ServerException | UserException | SchemaMismatchException e) {
 			arg0.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 500);
 			arg0.getResponseBody().close();
 		}
